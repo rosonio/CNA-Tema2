4c66656f4d4487e10120934ca6da24ac9ea1737e
Razvan <daniciucrazvan@yahoo.com>
2021-04-20 14:15:26 +0300

    Create ZodiacSign.java
    Created class for zodiac sign in which I define a zodiac object

---
+++ Server/src/main/java/Zodiac/ZodiacSign.java
@@ -0,0 +1,14 @@
+package Zodiac;
+
+public class ZodiacSign {
+    public String zodiacSign;
+    public String startingDate;
+    public String endDate;
+
+    public ZodiacSign( String zodie, String startingDate, String endDate )
+    {
+        this.endDate = endDate;
+        this.startingDate = startingDate;
+        this.zodiacSign = zodie;
+    }
+}
